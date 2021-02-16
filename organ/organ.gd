8b71ad35d16c407c8c6799a693cab60250d8718c
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-02-16 14:04:00 +0200

    Replace organ placeholder icons with better placeholders

--- organ/organ.gd
+++ organ/organ.gd
@@ -17,6 +17,7 @@ enum RESOURCE_TYPE {
 	NONE,
 	OXYGEN,
 	NUTRITION,
+	ACTIVITY
 }
 
 export (ORGAN_TYPE) var organ_type = ORGAN_TYPE.NONE
@@ -25,7 +26,7 @@ onready var _sprite: Sprite = $Sprite
 onready var _powerup_tween: Tween = $PowerupTween
 onready var _powerup: Sprite = $Powerup
 func _ready():
-	_sprite.region_rect.position.x = _get_sprite_frame() * 24
+	_sprite.region_rect.position.y = _get_sprite_frame() * 32
 	
 func show_powerup(powerup):
 	_powerup.visible = true
@@ -40,7 +41,17 @@ func _get_sprite_frame():
 	match organ_type:
 		ORGAN_TYPE.NONE:
 			return 0
+		ORGAN_TYPE.BRAIN:
+			return 1
+		ORGAN_TYPE.STOMACH:
+			return 2
+		ORGAN_TYPE.RIGHT_KIDNEY:
+			return 3
+		ORGAN_TYPE.LEFT_KIDNEY:
+			return 4
 		ORGAN_TYPE.LEFT_LUNG:
 			return 5
 		ORGAN_TYPE.RIGHT_LUNG:
 			return 6
+		ORGAN_TYPE.LIVER:
+			return 7
