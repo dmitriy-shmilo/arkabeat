0a3081675504eee5ec5903ff0f17a632844e79ed
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-02-16 15:20:54 +0200

    Fix inverted lung sprites. Minor spritesheet updates.

--- organ/organ.gd
+++ organ/organ.gd
@@ -25,9 +25,12 @@ export (ORGAN_TYPE) var organ_type = ORGAN_TYPE.NONE
 onready var _sprite: Sprite = $Sprite
 onready var _powerup_tween: Tween = $PowerupTween
 onready var _powerup: Sprite = $Powerup
+
+
 func _ready():
 	_sprite.region_rect.position.y = _get_sprite_frame() * 32
-	
+
+
 func show_powerup(powerup):
 	_powerup.visible = true
 	_powerup_tween.interpolate_property(_powerup, "position", \
@@ -36,6 +39,7 @@ func show_powerup(powerup):
 	_powerup_tween.start()
 	yield(_powerup_tween, "tween_all_completed")
 	_powerup.visible = false
+
 
 func _get_sprite_frame():
 	match organ_type:
@@ -49,9 +53,9 @@ func _get_sprite_frame():
 			return 3
 		ORGAN_TYPE.LEFT_KIDNEY:
 			return 4
-		ORGAN_TYPE.LEFT_LUNG:
-			return 5
 		ORGAN_TYPE.RIGHT_LUNG:
+			return 5
+		ORGAN_TYPE.LEFT_LUNG:
 			return 6
 		ORGAN_TYPE.LIVER:
 			return 7
