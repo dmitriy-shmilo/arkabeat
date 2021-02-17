4ce616df1941c94f2d4132c8cbbc75c9a9faeb16
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-02-17 11:50:08 +0200

    Add volume control

---
+++ persisted_settings.gd
@@ -0,0 +1,21 @@
+extends Node
+
+const MAX_VOLUME = 100.0
+
+var sfx_volume: float = MAX_VOLUME setget set_sfx_volume
+
+var _sfx_bus
+var _music_bus
+
+func _ready():
+	_sfx_bus = AudioServer.get_bus_index("Sfx")
+	_music_bus = AudioServer.get_bus_index("Music")
+
+func set_sfx_volume(value: float):
+	value = clamp(value, 0, MAX_VOLUME)
+	sfx_volume = value
+	_set_volume(_sfx_bus, value)
+
+func _set_volume(bus_idx: int, value: float):
+	AudioServer.set_bus_mute(bus_idx, sfx_volume == 0)
+	AudioServer.set_bus_volume_db(bus_idx, lerp(-40.0, 0, value / 100.0))
--- screens/settings/settings.gd
+++ screens/settings/settings.gd
@@ -1,11 +1,16 @@
 extends Control
 
 onready var _quit_button = $VBoxContainer/QuitButton
+onready var _sfx_slider = $VBoxContainer/HBoxContainer/Sfxslider
 
 signal back_pressed
 
 func _ready():
 	_quit_button.visible = _quit_button.visible and not OS.has_feature("HTML5")
+	_load_settings()
+
+func _load_settings():
+	_sfx_slider.value = PersistedSettings.sfx_volume
 
 
 func _on_BackButton_pressed():
@@ -14,3 +19,12 @@ func _on_BackButton_pressed():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+
+func _on_Settings_visibility_changed():
+	if self.visible:
+		_load_settings()
+
+
+func _on_Sfxslider_value_changed(value):
+	PersistedSettings.sfx_volume = value
